50a4c953f8019f95bd868e5b25e84ff9d2ee8376
Alelit4 <alelit4@gmail.com>
2021-04-18 16:31:56 +0100

    🔥 refactor: delete autogenerated Pokemon controller queries

--- src/controllers/pokemon.controller.ts
+++ src/controllers/pokemon.controller.ts
@@ -1,22 +1,5 @@
-import {
-  Count,
-  CountSchema,
-  Filter,
-  FilterExcludingWhere,
-  repository,
-  Where,
-} from '@loopback/repository';
-import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
-} from '@loopback/rest';
+import {Count, CountSchema, Filter, FilterExcludingWhere, repository} from '@loopback/repository';
+import {get, getModelSchemaRef, param, response} from '@loopback/rest';
 import {Pokemon} from '../models';
 import {MongodbPokemonRepository} from '../repositories';
 
@@ -26,34 +9,12 @@ export class PokemonController {
     public mongodbPokemonRepository : MongodbPokemonRepository,
   ) {}
 
-  @post('/pokemon')
-  @response(200, {
-    description: 'Pokemon model instance',
-    content: {'application/json': {schema: getModelSchemaRef(Pokemon)}},
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Pokemon, {
-            title: 'NewPokemon',
-            exclude: ['id'],
-          }),
-        },
-      },
-    })
-    pokemon: Omit<Pokemon, 'id'>,
-  ): Promise<Pokemon> {
-    return this.mongodbPokemonRepository.create(pokemon);
-  }
-
   @get('/pokemon/count')
   @response(200, {
     description: 'Pokemon model count',
     content: {'application/json': {schema: CountSchema}},
   })
   async count(
-    @param.where(Pokemon) where?: Where<Pokemon>,
   ): Promise<Count> {
     return this.mongodbPokemonRepository.count();
   }
@@ -76,25 +37,6 @@ export class PokemonController {
     return this.mongodbPokemonRepository.find(filter);
   }
 
-  @patch('/pokemon')
-  @response(200, {
-    description: 'Pokemon PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Pokemon, {partial: true}),
-        },
-      },
-    })
-    pokemon: Pokemon,
-    @param.where(Pokemon) where?: Where<Pokemon>,
-  ): Promise<Count> {
-    return this.mongodbPokemonRepository.updateAll(pokemon, where);
-  }
-
   @get('/pokemon/{id}')
   @response(200, {
     description: 'Pokemon model instance',
@@ -111,40 +53,4 @@ export class PokemonController {
     return this.mongodbPokemonRepository.findById(id, filter);
   }
 
-  @patch('/pokemon/{id}')
-  @response(204, {
-    description: 'Pokemon PATCH success',
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Pokemon, {partial: true}),
-        },
-      },
-    })
-    pokemon: Pokemon,
-  ): Promise<void> {
-    await this.mongodbPokemonRepository.updateById(id, pokemon);
-  }
-
-  @put('/pokemon/{id}')
-  @response(204, {
-    description: 'Pokemon PUT success',
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() pokemon: Pokemon,
-  ): Promise<void> {
-    await this.mongodbPokemonRepository.replaceById(id, pokemon);
-  }
-
-  @del('/pokemon/{id}')
-  @response(204, {
-    description: 'Pokemon DELETE success',
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.mongodbPokemonRepository.deleteById(id);
-  }
 }
